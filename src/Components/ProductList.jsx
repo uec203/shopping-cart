a0c888c22cf6f6a0300481b9b3aae6e6ed3a55cb
ROHITH <uec20342@gmail.com>
2025-02-22 19:15:17 +0530

    shopping_cart app

---
+++ src/Components/ProductList.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import './ProductList.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { addItemToCart } from './CartSlice';
+const ProductList = () => {
+
+    const dispatch = useDispatch();
+    const disabledProducts = useSelector(state => state.cart.cartItems);
+
+    const products = [
+        { id: 1, name: 'Product A', price: 60 },
+        { id: 2, name: 'Product B', price: 75 },
+        { id: 3, name: 'Product C', price: 30 },
+    ];
+
+    const handleAddToCart = product => {
+        dispatch(addItemToCart(product));
+    };
+
+    return (
+        <div className="product-list">
+            <h2 className="product-list-title">Products</h2>
+            <ul className="product-list-items">
+                {products.map(product => (
+                    <li key={product.id} className="product-list-item">
+                        <span>{product.name} - ${product.price}</span>
+                        <button className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+                            onClick={() => handleAddToCart(product)}
+                            disabled={disabledProducts.includes(product.id)}
+                        >
+                            Add to Cart
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ProductList;
